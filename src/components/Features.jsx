2946854ca9d29b911e57658557710dbdac744c90
lmndang-dev <lmndang@outlook.com>
2025-07-10 16:25:53 -0400

    svg issue

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,25 @@
 import "./Features.scss";
 
 const Features = () => {
-  return <section className="features">Features</section>;
+  return (
+    <section className="features">
+      <div className="feature">
+        <svg className="feature__icon">
+          <use xlinkHref="../assets/sprite.svg#icon-global"></use>
+        </svg>
+        <h4 className="heading-4">World's best luxury homes</h4>
+        <p className="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div className="feature">feature 2</div>
+      <div className="feature">feature 3</div>
+      <div className="feature">feature 4</div>
+      <div className="feature">feature 5</div>
+      <div className="feature">feature 6</div>
+    </section>
+  );
 };
 
 export default Features;

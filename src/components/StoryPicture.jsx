0ecef85071188549af84a6ff4b75c3532b4c5dc6
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-11 02:37:55 -0400

    lecture 110

--- src/components/StoryPicture.jsx
+++ src/components/StoryPicture.jsx
@@ -1,7 +1,18 @@
+import storyImg1 from "../assets/story-1.jpeg";
+import storyImg2 from "../assets/story-2.jpeg";
 import "./StoryPicture.scss";
 
 const StoryPicture = () => {
-  return <div className="story__pictures">Story Pictures</div>;
+  return (
+    <div className="story__pictures">
+      <img
+        src={storyImg1}
+        alt="couple with new house"
+        className="story__img--1"
+      />
+      <img src={storyImg2} alt="new house" className="story__img--2" />
+    </div>
+  );
 };
 
 export default StoryPicture;

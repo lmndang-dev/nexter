6a9d2d97237d9c15a11125cb44dca19b5792ae20
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-12 16:25:10 -0400

    header styling completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
+import Logo from "../assets/logo.png";
+import LogoBBC from "../assets/logo-bbc.png";
+import LogoForbes from "../assets/logo-forbes.png";
+import LogoTechCrunch from "../assets/logo-techcrunch.png";
+import LogoBusinnessInsider from "../assets/logo-bi.png";
+
 import "./Header.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <img src={Logo} alt="Nexter Logo" className="header__logo" />
+      <h3 className="heading-3">Your own home:</h3>
+      <h1 className="heading-1">The ultimate personal freedom</h1>
+      <button className="btn header__btn">View our properties</button>
+      <div className="header__seenon-text">Seen on</div>
+      <div className="header__seenon-logos">
+        <img src={LogoBBC} alt="Seen on logo 1" />
+        <img src={LogoForbes} alt="Seen on logo 2" />
+        <img src={LogoTechCrunch} alt="Seen on logo 3" />
+        <img src={LogoBusinnessInsider} alt="Seen on logo 4" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;

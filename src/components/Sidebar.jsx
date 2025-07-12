d8d69897d58ac819b89804f986f8c84d581fa1ce
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-12 14:51:58 -0400

    finished footer and sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <button className="nav-btn"></button>
+    </div>
+  );
 };
 
 export default Sidebar;

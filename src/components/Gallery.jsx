c2b41584dc666e5a8c97a6437b91e7bade9bb333
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-12 10:45:59 -0400

    finished gallery section styling

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,20 @@
+import GALLERY from "../data/gallery";
+
 import "./Gallery.scss";
 
 const Gallery = () => {
-  return <section className="gallery">Gallery</section>;
+  return (
+    <section className="gallery">
+      {GALLERY.map((item, index) => (
+        <figure
+          className={`gallery__item gallery__item--${index + 1}`}
+          key={item.name}
+        >
+          <img src={item.src} alt={item.name} className="gallery__img" />
+        </figure>
+      ))}
+    </section>
+  );
 };
 
 export default Gallery;
---
+++ src/data/gallery.js
@@ -0,0 +1,33 @@
+import Gal1 from "../assets/gal-1.jpeg";
+import Gal2 from "../assets/gal-2.jpeg";
+import Gal3 from "../assets/gal-3.jpeg";
+import Gal4 from "../assets/gal-4.jpeg";
+import Gal5 from "../assets/gal-5.jpeg";
+import Gal6 from "../assets/gal-6.jpeg";
+import Gal7 from "../assets/gal-7.jpeg";
+import Gal8 from "../assets/gal-8.jpeg";
+import Gal9 from "../assets/gal-9.jpeg";
+import Gal10 from "../assets/gal-10.jpeg";
+import Gal11 from "../assets/gal-11.jpeg";
+import Gal12 from "../assets/gal-12.jpeg";
+import Gal13 from "../assets/gal-13.jpeg";
+import Gal14 from "../assets/gal-14.jpeg";
+
+const GALLERY = [
+  { name: "gallery image 1", src: Gal1 },
+  { name: "gallery image 2", src: Gal2 },
+  { name: "gallery image 3", src: Gal3 },
+  { name: "gallery image 4", src: Gal4 },
+  { name: "gallery image 5", src: Gal5 },
+  { name: "gallery image 6", src: Gal6 },
+  { name: "gallery image 7", src: Gal7 },
+  { name: "gallery image 8", src: Gal8 },
+  { name: "gallery image 9", src: Gal9 },
+  { name: "gallery image 10", src: Gal10 },
+  { name: "gallery image 11", src: Gal11 },
+  { name: "gallery image 12", src: Gal12 },
+  { name: "gallery image 13", src: Gal13 },
+  { name: "gallery image 14", src: Gal14 },
+];
+
+export default GALLERY;

7f848955b11c5c454926a34867bf2a0025d9c731
Le Minh Nhat Dang <leminhnhat.dang@Les-MacBook-Pro.local>
2025-07-12 17:03:45 -0400

    reltors section styling completed:

--- src/components/Realtors.jsx
+++ src/components/Realtors.jsx
@@ -1,7 +1,34 @@
+import { Fragment } from "react";
+
+import REALTORS from "../data/realtors";
+
 import "./Realtors.scss";
 
 const Realtors = () => {
-  return <div className="realtors">Top 3 realtors</div>;
+  return (
+    <div className="realtors">
+      <h3 className="heading-3">Top 3 realtors</h3>
+      <div className="realtors__list">
+        {REALTORS.map(({ imgSrc, name, numberOfHouseSold }, index) => {
+          return (
+            <Fragment key={index}>
+              <img
+                src={imgSrc}
+                alt={`${name} profile picture`}
+                className="realtors__img"
+              />
+              <div className="realtors__details">
+                <h4 className="heading-4 heading-4--light">{name}</h4>
+                <p className="realtors__sold">
+                  {numberOfHouseSold} houses sold
+                </p>
+              </div>
+            </Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Realtors;
---
+++ src/data/realtors.js
@@ -0,0 +1,23 @@
+import Realtor1 from "../assets/realtor-1.jpeg";
+import Realtor2 from "../assets/realtor-2.jpeg";
+import Realtor3 from "../assets/realtor-3.jpeg";
+
+const REALTORS = [
+  {
+    imgSrc: Realtor1,
+    name: "Erik Feinman",
+    numberOfHouseSold: 245,
+  },
+  {
+    imgSrc: Realtor2,
+    name: "Kim Brown",
+    numberOfHouseSold: 212,
+  },
+  {
+    imgSrc: Realtor3,
+    name: "Toby Ramsey",
+    numberOfHouseSold: 198,
+  },
+];
+
+export default REALTORS;
